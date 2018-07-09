e5676dcb3da6b9bcb21ef0b22cea8cd72b3e6fd0
Friedrich <friedrich-gi@outlook.com>
2018-07-09 11:51:08 +0200

    switched to difflib

--- src/main/java/at/aau/Main.java
+++ src/main/java/at/aau/Main.java
@@ -1,8 +1,22 @@
 package at.aau;
 
+
+import difflib.*;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<String> original = fileToLines("originalFile.txt");
+        List<String> revised  = fileToLines("revisedFile.xt");
+
+        // Compute diff. Get the Patch object. Patch is the container for computed deltas.
+        Patch patch = DiffUtils.diff(original, revised);
+
+        for (Delta delta: patch.getDeltas()) {
+            System.out.println(delta);
+        }
     }
 }
