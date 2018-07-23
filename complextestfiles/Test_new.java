256837b74d2090a753e830799c0e7ad8fdb5304b
Christoph Wedenig <christoph@wedenig.org>
2018-07-23 11:31:46 +0200

    Added a more complex test

---
+++ complextestfiles/Test_new.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class TestClass extends SuperClass {
+
+    public String newVar = "so new";
+
+    private void lol()
+    {
+        System.out.println("nix");
+    }
+
+    public TestClass()
+    {
+        int varUpd = 4444321;
+    }
+}
